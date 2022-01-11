92c3218898549d4bb48f5f60920a434a2a2426bc
MrAzuka <johntwice8@gmail.com>
2022-01-11 12:16:48 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            firstName: 'Olisemelie',
+            lastName: 'Azuka',
+            email: 'zuks@gmail.com',
+            age: '20',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/75.jpg'
+        }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api/')
+            const {results} = await res.json()
+
+            this.firstName =  results[0].name.first
+            this.lastName =  results[0].name.last
+            this.email = results[0].email
+            this.gender =  results[0].gender
+            this.age =  results[0].dob.age
+            this.picture =  results[0].picture.large
+        }
+    }
+})
+
+
+app.mount('#app')
